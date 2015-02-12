63f0a348dd6da50c93378a1b3567256673d091ec
Jack Eller <admin@kilobear.com>
2015-02-12 08:10:05 -0500

    Working on item file creation

---
+++ owner.java
@@ -0,0 +1,61 @@
+//Creates items, manages stock
+import java.util.Scanner;
+public class owner{
+
+
+    public static void main(String[] args){
+
+	System.out.println("Welcome admin!");
+
+	System.out.println("What would you like to do?");
+
+	Scanner scan = new Scanner(System.in);
+
+	boolean running = true;
+
+	while(running){
+
+	System.out.println("1.\tCreate new item\n2.\tEdit an item\n3.\tDelete an item\n4.\tQuit");
+
+	int choice = Scan.nextInt();
+
+	switch(choice){
+	    
+	case 1: 
+
+	    break;
+	    
+	case 2: 
+
+	    break;
+
+	case 3: 
+
+	    break;
+
+	case 4:
+
+	    System.out.println("Quit... Goodbye";
+
+			       running = false;
+
+	default:
+
+
+
+	    break;
+
+	}
+
+      }
+    }
+
+
+	public itemload(){
+
+
+	}
+
+
+}
+
