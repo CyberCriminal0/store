f8ae8e499651951e957eecb3120133f36d6c118e
Jack Eller <admin@kilobear.com>
2015-02-11 23:13:49 -0500

    Added store logic

---
+++ store.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class store{
+
+    public static void main(String[] args){
+
+	System.out.println("Loading Store " + "Timestamp " + System.currentTimeMillis() + "...");
+	
+	Scanner scan = new Scanner(System.in);
+
+	System.out.print("\007");
+
+	System.out.println("Welcome to Store!");
+
+	System.out.println("Please input your name");
+
+	String username = scan.nextLine();
+	
+	System.out.println("Welcome " + username + " are you a returning user? y/n");
+
+	String returning = scan.nextLine();
+
+	boolean returningB;
+
+	if(returning.equalsIgnoreCase("y")){
+
+		returningB = true;
+	    
+	    }else{
+		
+		returningB = false;
+
+	    }
+
+    }
+
+
+
+}
